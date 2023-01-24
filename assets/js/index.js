19e87cd0b228f09a079fa3acebcc29c6cac8fcbb
meri <mariana_asavoaiei@outlook.com>
2023-01-24 12:33:44 +0000

    looped through the questions array in order to set the content for the current question title. looped through the answers array to get the answer options.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,3 @@
-
 //Created variables containing the ids of the elements so that we can grab them with JS.
 let questionTitle = document.querySelector("#question-title");
 let choicesEl = document.querySelector("#choices");
@@ -8,36 +7,49 @@ let submitButton = document.querySelector("#submit");
 // Created an array of objects containing the questions, answers and correct answer.
 let questions = [
   {
-    question1: "",
+    question: "",
     answers: ["option1", "option2", "option 3"],
-    correctOption: 1
+    correctOption: 1,
   },
   {
-    question2: "",
+    question: "",
     answers: ["option1", "option2", "option 3"],
-    correctOption: 3
+    correctOption: 3,
   },
   {
-    question3: "",
+    question: "",
     answers: ["option1", "option2", "option 3"],
-    correctOption: 2
+    correctOption: 2,
   },
   {
-    question4: "",
+    question: "",
     answers: ["option1", "option2", "option 3"],
-    correctOption: 2
+    correctOption: 2,
   },
   {
-    question5: "",
+    question: "",
     answers: ["option1", "option2", "option 3"],
-    correctOption: 1
+    correctOption: 1,
   },
 ];
 
 // Added an event listener to the start button so that when it's clicked, the div with the #start-screen is hidden and the div with the id #questions is revealed.
 startButton.addEventListener("click", function (event) {
   if (event.target.matches("#start")) {
-    document.querySelector("#start-screen").setAttribute("class","hide");
+    document.querySelector("#start-screen").setAttribute("class", "hide");
     document.querySelector("#questions").classList.remove("hide");
   }
 });
+
+//We are setting the text content of the h2 to be the current question.
+for (let i = 0; i < questions.length; i++) {
+  questionTitle = questions[i].question;
+  questionTitle.textContent = questions[i].question;
+  console.log(questionTitle);
+}
+
+// We are now looping through the answers
+for (let i = 0; i < questions.length; i++) {
+  const answer = questions[i].answers[i];
+  console.log(answer);
+}
